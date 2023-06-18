b15f0036eccf38a7aee6b59f9bf420e6b7554549
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-06-18 17:35:11 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 =31;
+let num2=2;
+
+let num3=11
+let num4=7
+
+
+const multiply = num1 * num2;
+
+const random = Math.floor((Math.random() *10)+1)
+
+const mod = (Math.floor(num3/num4)+3)
+
+const max= Math.max(5,10,15,20)
+
+console.log(mod)
